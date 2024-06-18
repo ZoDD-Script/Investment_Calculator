a93b14deafda554fc89523765177cf9db612f004
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-18 16:11:31 +0100

    computing values

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,11 @@
+import { calculateInvestmentResults } from "../util/investment"
+
 export default function Result({ input }) {
+  const resultData = calculateInvestmentResults(input)
+
   return(
     <section id="result">
-      {console.log(input)}
+      {console.log(resultData)}
       <div>Results...</div>
     </section>
   )
