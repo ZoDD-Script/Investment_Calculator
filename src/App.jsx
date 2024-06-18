97fff3d65fb82ca1ce17e68805077d1cc9af3aad
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-06-18 16:04:30 +0100

    Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from "react"
 import Header from "./components/Header"
+import Result from "./components/Results"
 import UserInput from "./components/UserInput"
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  })
+
+  function handleUserInput(inputIdentifier, newValue) {
+    setUserInput(prevInput => {
+      return {
+        ...prevInput,
+        [inputIdentifier]: newValue
+      }
+    })
+  }
+
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleUserInput} />
+      <Result input={userInput} />
     </>
   )
 }
---
+++ src/components/Results.jsx
@@ -0,0 +1,8 @@
+export default function Result({ input }) {
+  return(
+    <section id="result">
+      {console.log(input)}
+      <div>Results...</div>
+    </section>
+  )
+}
